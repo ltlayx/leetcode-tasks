16277d5c57ac78a2dab1c58d715fda2a2fdc5ef9
ltlayx <litailang_cn@126.com>
2018-08-09 16:26:40 +0800

    Problem 107:Binary Tree Level Order Traversal II

--- src/Binary_Tree_Level_Order_Traversal_II_107.java
+++ src/Binary_Tree_Level_Order_Traversal_II_107.java
@@ -1,6 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *    3
+ *   / \
+ *  9  20
+ *  /  \
+ * 15   7
+ * return its bottom-up level order traversal as:
+ * [
+ * [15,7],
+ * [9,20],
+ * [3]
+ * ]
+ */
 public class Binary_Tree_Level_Order_Traversal_II_107 {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         int treeHeight = searchTreeHeight(root);
