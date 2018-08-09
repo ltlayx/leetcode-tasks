1ada09f921614652b85bb3e37e57bd7bcf1eef33
ltlayx <litailang_cn@126.com>
2018-08-09 10:01:22 +0800

    Problem 88:Merge Sorted Array

---
+++ src/Merge_Sorted_Array_88.java
@@ -0,0 +1,42 @@
+/**
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
+ * Note:
+ *
+ * The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+ * Example:
+ *
+ * Input:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ *
+ * Output: [1,2,2,3,5,6]
+ */
+
+public class Merge_Sorted_Array_88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n == 0){
+            return;
+        }
+        if(m == 0){
+            for(int i = 0; i < n; i++){
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+        int mergeCount = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = m + mergeCount - 1; j >= 0 ; j--){
+                if(nums1[j] > nums2[i]){
+                    nums1[j + 1] = nums1[j];
+                    nums1[j] = nums2[i];
+                }else{
+                    nums1[j + 1] = nums2[i];
+                    break;
+                }
+            }
+            mergeCount++;
+        }
+    }
+}
